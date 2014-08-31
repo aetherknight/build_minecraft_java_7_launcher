f52c0ff5fbbd0ea9e186d2d87ca08f5ad04eb5b8
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2014-08-30 20:25:46 -0700

    Wrapping the script in an AppleScript application

--- Build Java 7-compatible Minecraft Launcher.app/Contents/Resources/builder.sh
+++ Build Java 7-compatible Minecraft Launcher.app/Contents/Resources/builder.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # Usage: sh build_minecraft_java_7_launcher.sh /Applications/Minecraft.app /Applications/Minecraft-better-launcher.app
 
-if [ $# -ne 2 ] ; then
-    echo "Usage: $0 /Applications/Minecraft.app /Applications/Minecraft-better-launcher.app"
-    exit 1
-fi
+#if [ $# -ne 2 ] ; then
+#    echo "Usage: $0 /Applications/Minecraft.app /Applications/Minecraft-better-launcher.app"
+#    exit 1
+#fi
 
 ORIG_LAUNCHER=$1
 DEST_LAUNCHER=$2
 
-RESOURCES=`dirname $0`
+RESOURCES=`dirname "$0"`
 
 mkdir -p "${DEST_LAUNCHER}"
 mkdir -p "${DEST_LAUNCHER}/Contents"
+echo "${RESOURCES}/Info.plist" 
 cp "${RESOURCES}/Info.plist" "${DEST_LAUNCHER}/Contents/"
 
 mkdir -p "${DEST_LAUNCHER}/Contents/MacOS"
@@ -23,4 +24,4 @@ cp -R "${ORIG_LAUNCHER}/Contents/Resources/en.lproj"     "${DEST_LAUNCHER}/Conte
 cp -R "${ORIG_LAUNCHER}/Contents/Resources/favicon.icns" "${DEST_LAUNCHER}/Contents/Resources/"
 cp -R "${ORIG_LAUNCHER}/Contents/Resources/Java"         "${DEST_LAUNCHER}/Contents/Resources/"
 
-echo "Done."
+echo Done.
