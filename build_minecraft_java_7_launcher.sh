0a580a16c16533d8d0fe8446495b53759be35313
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2014-08-30 18:45:24 -0700

    Initial commit of a script to build a Minecraft launcher for Java 7
    It requires the original (Apple-Java-6-only) launcher to get the
    bootstrap.jar, icon, and localization info.

---
+++ build_minecraft_java_7_launcher.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Usage: sh build_minecraft_java_7_launcher.sh /Applications/Minecraft.app /Applications/Minecraft-better-launcher.app
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 /Applications/Minecraft.app /Applications/Minecraft-better-launcher.app"
+    exit 1
+fi
+
+ORIG_LAUNCHER=$1
+DEST_LAUNCHER=$2
+
+RESOURCES=`dirname $0`
+
+mkdir -p "${DEST_LAUNCHER}"
+mkdir -p "${DEST_LAUNCHER}/Contents"
+cp "${RESOURCES}/Info.plist" "${DEST_LAUNCHER}/Contents/"
+
+mkdir -p "${DEST_LAUNCHER}/Contents/MacOS"
+cp -p "${RESOURCES}/MinecraftLauncher" "${DEST_LAUNCHER}/Contents/MacOS/"
+
+mkdir -p "${DEST_LAUNCHER}/Contents/Resources"
+cp -R "${ORIG_LAUNCHER}/Contents/Resources/en.lproj"     "${DEST_LAUNCHER}/Contents/Resources/"
+cp -R "${ORIG_LAUNCHER}/Contents/Resources/favicon.icns" "${DEST_LAUNCHER}/Contents/Resources/"
+cp -R "${ORIG_LAUNCHER}/Contents/Resources/Java"         "${DEST_LAUNCHER}/Contents/Resources/"
+
+echo "Done."
